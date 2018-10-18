e796587cca58b8323581adc0fd362d95ec577f83
daliansky <daliansky2008@gmail.com>
2018-10-18 10:33:23 +0800

    Site updated: 2018-10-18 10:33:22

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Thu Oct 18 2018 09:07:41 GMT+0800 (China Standard Time)
+// build time:Thu Oct 18 2018 10:33:15 GMT+0800 (China Standard Time)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
