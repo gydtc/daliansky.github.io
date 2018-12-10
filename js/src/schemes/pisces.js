871c9c6c0bfd4b8848ec9b30a6bd28ace736f166
daliansky <daliansky2008@gmail.com>
2018-12-10 11:43:34 +0800

    Site updated: 2018-12-10 11:43:33

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Mon Dec 10 2018 09:38:03 GMT+0800 (中国标准时间)
+// build time:Mon Dec 10 2018 11:43:26 GMT+0800 (中国标准时间)
 $(document).ready(function(){var e=$(".sidebar-inner");function t(){var t,i,a=$(".header-inner").height()+CONFIG.sidebar.offset,f=(t=$(".footer-inner"),i=t.outerHeight(!0)-t.outerHeight(),t.outerHeight(!0)+i);a+($("#sidebar").height()+NexT.utils.getSidebarb2tHeight())<$("#content").height()&&e.affix({offset:{top:a-CONFIG.sidebar.offset,bottom:f}}),function(e){return $("#sidebar").css({"margin-top":e})}(a).css({"margin-left":"initial"})}t(),window.matchMedia("(min-width: 991px)").addListener(function(i){i.matches&&($(window).off(".affix"),e.removeData("bs.affix").removeClass("affix affix-top affix-bottom"),t())})});
 //rebuild by neat 
--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Mon Dec 10 2018 09:38:03 GMT+0800 (中国标准时间)
+// build time:Mon Dec 10 2018 11:43:26 GMT+0800 (中国标准时间)
 $(document).ready(function(){var o,i=window.location.href.replace(window.location.origin,"");if($(window).on("scroll",function(){clearTimeout(o),o=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+i,{expires:365,path:""})},250)}),void 0!==Cookies.get("scroll-cookie")){var e=Cookies.get("scroll-cookie").split("|");e[1]==i&&$(window).scrollTop(e[0])}});
 //rebuild by neat 
